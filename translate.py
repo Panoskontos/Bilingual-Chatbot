cc15a6f0ee2b0e4223d1157cce0190101f09fec8
Panagiotis Kontos <65974766+Panoskontos@users.noreply.github.com>
2021-10-23 05:55:50 +0300

    Add files via upload
    translator

---
+++ translate.py
@@ -0,0 +1,14 @@
+from googletrans import Translator
+
+translater = Translator()
+
+'''
+Just testing my translator
+
+
+out = translater.translate('Γειααα σου τι κανεις?')
+
+translator.detect('이 문장은 한글로 쓰여졌습니다.')
+
+print(out.text)
+'''
